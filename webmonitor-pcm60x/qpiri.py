5303b3b96041eedf1610b84fc0bce4c18404af81
solarsnoop <40702156+solarsnoop@users.noreply.github.com>
2018-08-21 23:20:04 +0200

    Update qpiri.py

--- webmonitor-pcm60x/qpiri.py
+++ webmonitor-pcm60x/qpiri.py
@@ -5,7 +5,6 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)): 
     ssl._create_default_https_context = ssl._create_unverified_context
 QPIRI= "\x51\x50\x49\x52\x49\xF8\x54\x0D"
-apikey="dd37f0cfa7c8ccef5b4fa5fa81282650"
 ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=2)
 ser.write(QPIRI)
 result = ser.read(70)
