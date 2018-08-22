f33d7a952a916c21ca7d4e17c7e1781192c782b0
solarsnoop <40702156+solarsnoop@users.noreply.github.com>
2018-08-22 15:21:55 +0200

    Update qpiri.py

--- webmonitor-pcm60x/qpiri.py
+++ webmonitor-pcm60x/qpiri.py
@@ -1,20 +1,20 @@
+ #!/usr/bin/env python
 import time
 import serial
 import os, ssl
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)): 
-    ssl._create_default_https_context = ssl._create_unverified_context
 QPIRI= "\x51\x50\x49\x52\x49\xF8\x54\x0D"
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=2)
-ser.write(QPIRI)
-result = ser.read(70)
-ser.read()
-ser.read()
-print result
-ser.close()
-fobj_in = open("/home/pi/pyhton/qpiri.txt")
-fobj_out = open("/home/pi/pyhton/qpiri.txt","w")
-i = 1
-fobj_out.write(result)
+fobj_in = open("qpiri.txt")
+i = 0
+while i < 1:
+    ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=5)
+    if serial.serialutil.SerialException and ser.isOpen():
+       ser.write(QPIRI)
+       result = ser.read(68)
+       if '(' in result:
+          fobj_out = open("qpiri.txt","w")
+          fobj_out.write(result)
+          fobj_out.close()
+          print "success"
+          i = 1
+    ser.close()
 fobj_in.close()
-fobj_out.close()
