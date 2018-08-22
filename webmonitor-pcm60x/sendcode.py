af27e8e19dae6ae67e6bf94cb84dda9c41f61cc9
solarsnoop <40702156+solarsnoop@users.noreply.github.com>
2018-08-22 15:22:28 +0200

    Update sendcode.py

--- webmonitor-pcm60x/sendcode.py
+++ webmonitor-pcm60x/sendcode.py
@@ -2,17 +2,23 @@ import time
 import binascii
 import serial
 import os, ssl
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=2)
-filename = '/home/pi/pyhton/transfer.txt'
+filename = 'transfer.txt'
 with open(filename, 'rb') as f:
-    content = f.read()
+     content = f.read()
 if content == "504254303274C0D":
    qcode = "\x50\x42\x54\x30\x32\x74\x0C\x0D"
 else:
     qcode = content.decode("hex")
-print qcode
-ser.write(qcode)
-result = ser.read(5)
-print result
-ser.close()
-f.close()
+i = 0
+while i < 1:
+      ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=5)
+      if serial.serialutil.SerialException and ser.isOpen():
+         ser.write(qcode)
+         result = ser.read(5)
+         if '(A' in result:
+            print "success"
+            i = 1
+         if '(N' in result:
+            print "unsuccessful"
+            i = 1
+      ser.close()
