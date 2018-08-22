59a9712b206cdabebcef525857d2115568856a3a
solarsnoop <40702156+solarsnoop@users.noreply.github.com>
2018-08-22 15:21:15 +0200

    Update qpigs.py
    verify success or not

--- webmonitor-pcm60x/qpigs.py
+++ webmonitor-pcm60x/qpigs.py
@@ -1,16 +1,20 @@
+ #!/usr/bin/env python
 import time
 import serial
 import os, ssl
 QPIGS = "\x51\x50\x49\x47\x53\xB7\xA9\x0D"
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=2)
-ser.write(QPIGS)
-result = ser.read(70)
-ser.read()
-ser.read()
-ser.close()
 fobj_in = open("qpigs.txt")
-fobj_out = open("qpigs.txt","w")
-i = 1
-fobj_out.write(result)
+i = 0
+while i < 1:
+    ser = serial.Serial(port='/dev/ttyUSB0',baudrate=2400,timeout=5)
+    if serial.serialutil.SerialException and ser.isOpen():
+       ser.write(QPIGS)
+       result = ser.read(68)
+       if '(' in result:
+          fobj_out = open("qpigs.txt","w")
+          fobj_out.write(result)
+          fobj_out.close()
+          print "success"
+          i = 1
+    ser.close()
 fobj_in.close()
-fobj_out.close()
