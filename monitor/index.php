307daa72199ab5e3f272b92054fcf95334250029
solarsnoop <40702156+solarsnoop@users.noreply.github.com>
2018-08-30 15:46:03 +0200

    Update index.php

--- monitor/index.php
+++ monitor/index.php
@@ -1,49 +1,26 @@
-<?php
-if (isset($_POST['subject2'])=='refresh') {
-$check = shell_exec("sudo python qpigs.py");
-IF ($check!='')
-   {
-   echo "<center> refresh: ".$check;
-   }
-else
-  {
-   echo "<center> refresh: not successfull";
-  }
-}
-?>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<style>
-.styled {
-    border: 0;
-    line-height: 2.5;
-    padding: 0 20px;
-    font-size: 1rem;
-    text-align: center;
-    color: #fff;
-    text-shadow: 1px 1px 1px #000;
-    border-radius: 10px;
-    background-color: rgba(220, 0, 0, 1);
-    background-image: linear-gradient(to top left,
-                                      rgba(0, 0, 0, .2),
-                                      rgba(0, 0, 0, .2) 30%,
-                                      rgba(0, 0, 0, 0));
-    box-shadow: inset 2px 2px 3px rgba(255, 255, 255, .6),
-                inset -2px -2px 3px rgba(0, 0, 0, .6);
-}
-
-.styled:hover {
-    background-color: rgba(255, 0, 0, 1);
-}
-
-.styled:active {
-    box-shadow: inset -2px -2px 3px rgba(255, 255, 255, .6),
-                inset 2px 2px 3px rgba(0, 0, 0, .6);
-}
-</style>
+<body>
+<center><p>  
+<title>Dashboard</title>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>    
+<script type="text/javascript">
+     $(document).ready(function() {
+       $("#refresh").load("refresh.php");
+       var refreshId = setInterval(function() {
+          $("#refresh").load('refresh.php?' + 1*new Date());
+       }, 1000);
+    });
+</script>
 </head>
+<body>
+   <div id="refresh" style="text-align:center;">
+<?php
+  include ("refresh.php");
+?>
+</div>
+</body>
 <?php
 define( 'CRC16POLYN', 0x1021 );
 define( 'CRC16POLYI', 0x8408 );
@@ -96,14 +73,38 @@ for ($i = 0; $i < strlen($ascii); $i++) {
     $hexasc.=$byte;
   }
 $out=$hexasc.$crc1.$crc2.'0D';
-echo '<center>'.$setnew;
+echo '<hr><center>'.$setnew;
 echo '<br>HEXCODE: '.$out;
 echo '</center><br>';
-file_put_contents("transfer.txt", $out); 
-$check = shell_exec("sudo python sendcode.py");
+file_put_contents("/var/www/html/monitor/transfer.txt", $out);
+$check = shell_exec("sudo python2 /var/www/html/monitor/sendcode.py");
+$check = $setnew;
 IF ($check!='')
    {
-   echo "<center> setup: ".$check;
+   echo "<center> setup successful: ".$check;
+   $filename = '/var/www/html/monitor/qpiri.txt';
+   $file = file_get_contents($filename);
+   $remove="(";
+   $file=str_replace($remove, "", $file); 
+   $einzeln = explode(' ', $file);
+   if ($_POST['batsysp'] <> 0 and $_POST['batsysp'] <> 11){
+       $einzeln[1]=$_POST['batsysp']*12;
+       }
+   if ($_POST['cursysp'] <> 11){
+       $einzeln[2]=$_POST['cursysp'];
+       }
+    if ($_POST['bulkp'] <> $_POST['bulkv']){
+        $einzeln[3]=$_POST['bulkp']/$_POST['factorv'];
+        }
+    if ($_POST['floatp'] <> $_POST['floatv']){
+        $einzeln[4]=$_POST['floatp']/$_POST['factorv'];
+        }
+   if ($_POST['battypep'] <> 11){
+       $einzeln[5]="0".$_POST['battypep'];
+       }
+    $outf="(".$einzeln[0]." ".$einzeln[1]." ".$einzeln[2]." ".$einzeln[3]." ".$einzeln[4]." ".$einzeln[5];
+    file_put_contents($filename, $outf); 
+    echo "<hr>";
    }
 else
   {
@@ -214,35 +215,8 @@ $bulkp=0;
 $floatp=0;
 $battypep=0;
 ### experimental version not for use yet
-if (isset($_POST['subject'])) {
-  if ($_POST['subject']=='setup') {
-  $check = shell_exec("sudo python qpiri.py");
-  if ($check!='')
-     {
-     echo "<center> reload settings: ".$check;
-     }
-  else
-    {
-    echo "<center> reload settings: not successfull";
-    }
-  }
-}
-$filename = 'qpiri.txt';
-$filename2 = 'qpigs.txt';
+$filename = '/var/www/html/monitor/qpiri.txt';
 $timestamp=time();
-if (file_exists($filename2)) 
-   {  
-    $file = file_get_contents($filename2);
-   }
-$remove="(";
-$file=str_replace($remove, "", $file); 
-$einzeln = explode(' ', $file);
-###################### STATUS ################################
-$pvvolt = floatval($einzeln[0]); ### PV Voltage
-$pvwatts= floatval($einzeln[5]); ### PV Watt
-$volt = floatval($einzeln[1]); ### Batt Voltage
-$current = floatval($einzeln[2]); ### Batt Ampere
-$ctemp = floatval($einzeln[6]); ### Charger Temp
 if (file_exists($filename))
    {  
     $file = file_get_contents($filename);
@@ -265,172 +239,8 @@ $battm1 = 122/10*$factor;
 $batth = 155/10*$factor;
 $bathm2 = 125/10*$factor;
 $batt1 = 11*$factor;
-#### SOC Volt based
-     IF ($volt/$factor >128/10)
-        {
-        $ladestatus=100;  
-        }
-ELSE If ($volt/$factor <115/10)
-        {
-        $ladestatus=0;  
-     	}
-     ELSE
-        {
-		$ladestatus=($volt/$factor-11.5)/0.013;  
-     	}
-$ladestatus=floor($ladestatus);
-if ($ladestatus>75){$color="green";$hexcol="7CFC00";$battgraf="battery1.jpg";}
-elseif ($ladestatus>50){$color="gold";$hexcol="#FFFF00";$battgraf="battery2.jpg";}
-elseif ($ladestatus>30){$color="orange";$hexcol="#FFFF00";$battgraf="battery3.jpg";}
-else {$color="red";$hexcol="#FF0000";$battgraf="battery4.jpg";}
 ?>
-<body>
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Volt', <?php echo $volt;?>]
-        ]);
-
-        var options = {
-          width: 120, height: 120,
-          greenFrom: <?php echo 12*$factor;?>, greenTo: <?php echo 15*$factor;?>,
-          yellowFrom: <?php echo 11*$factor;?>, yellowTo: <?php echo 12*$factor;?>,
-          redFrom: 0, redTo: <?php echo 11*$factor;?>,
-          max: <?php echo 15*$factor;?>,
-          minorTicks: 8
-        };
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div3'));
-        chart.draw(data, options);
-
-        setInterval(function() {
-          data.setValue(0, 1, <?php echo $volt;?>);
-          chart.draw(data, options);
-        },0);
-      }
-     </script>
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Ampere', <?php echo $current;?>]
-        ]);
-
-        var options = {
-          width: 120, height: 120,
-          greenFrom: 0, greenTo: 200,
-          yellowFrom:-20, yellowTo: 0,
-          redFrom:-200, redTo: -20,
-          max: 200,
-		  min: -200,
-          minorTicks: 8
-        };
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div1'));
-        chart.draw(data, options);
-
-        setInterval(function() {
-          data.setValue(0, 1, <?php echo $current;?>);
-          chart.draw(data, options);
-        },0);
-      }
-     </script>
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Watt', <?php echo $pvwatts;?>]
-        ]);
-
-        var options = {
-          width: 120, height: 120,
-          greenFrom: 300, greenTo: 3500,
-          yellowFrom:100, yellowTo: 300,
-          redFrom:0, redTo: 100,
-          max: 3500,
-          minorTicks: 8
-        };
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div2'));
-        chart.draw(data, options);
-
-        setInterval(function() {
-          data.setValue(0, 1, <?php echo $pvwatts;?>);
-          chart.draw(data, options);
-        },0);
-      }
-     </script> 
-
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Label', 'Value'],
-          ['Volt', <?php echo $pvvolt;?>]
-        ]);
-
-        var options = {
-          width: 120, height: 120,
-          greenFrom: 50, greenTo: 115,
-          yellowFrom: 0, yellowTo: 50,
-          redFrom: 116, redTo: 145,
-          max: 150,
-          minorTicks: 8
-        };
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div4'));
-        chart.draw(data, options);
-
-        setInterval(function() {
-          data.setValue(0, 1, <?php echo $pvvolt;?>);
-          chart.draw(data, options);
-        },0);
-      }
-     </script> 
-  </body>
-<hr>
-<p><center>
-<h1><u><center>PCM60X MONITOR</u></h><br></p>
-<table width="90%" align="center" BORDER="1"><tr>
-<td bgcolor=#e3e3e3 align="center" width="33%">Time: <?php echo $uhrzeit;?></td>
-<td bgcolor=#e3e3e3 align="center" width="33%">Charger Temp: <?php echo $ctemp;?> C</td>
-</tr>  
-</table>
-<hr><center><h4>Status</h>
-<table WIDTH=90% cellspacing="10" cellpadding="20"><tr><td>
-    <table align="center" BORDER="1"><tr>
-	<td bgcolor=#CCFFCC align="center"><font size="4">Voltage Battery</td>
-	<td bgcolor=#CCFFCC align="center"><font size="4">Ampere Battery</td>
-    <td bgcolor=#CCFFCC align="center"><font size="4">PV Watt</td>
-	<td bgcolor=#CCFFCC align="center"><font size="4">PV Voltage</td>
-    <td bgcolor=#CCFFCC align="center"><font size="4">SOC Voltage based</td>
-	</tr><tr>
-    <td><div id="chart_div3" style="width: 120px; height: 120px;"></div></td>
-    <td><div id="chart_div1" style="width: 120px; height: 120px;"></div></td>
-    <td><div id="chart_div2" style="width: 120px; height: 120px;"></div></td>
-    <td><div id="chart_div4" style="width: 120px; height: 120px;"></div></td>
-    <td align=center><img src="<?php echo $battgraf;?>" width="60" height="120"></td>
-	</tr><tr>
-	<td bgcolor=#CCFFCC align="center"><font size="4"><?php echo $volt." V";?></td>
-	<td bgcolor=#CCFFCC align="center"><font size="4"><?php echo $current." A";?></td>
-    <td bgcolor=#CCFFCC align="center"><font size="4"><?php echo $pvwatts." W";?></td>
-	<td bgcolor=#CCFFCC align="center"><font size="4"><?php echo $pvvolt." V";?></td>
-    <td bgcolor=#CCFFCC align="center"><font size="4"><?php echo $ladestatus." %";?></td>
-	</tr>
-	</table></td></tr></table>
-<hr><center><h4>Settings</h>
+</p><p><center><h1>Settings</h>
 <table WIDTH=90% cellspacing="10" cellpadding="20"><tr><td>
     <form align=center method="post">
     <table align="center" BORDER="1"><tr>
@@ -494,4 +304,8 @@ else {$color="red";$hexcol="#FF0000";$battgraf="battery4.jpg";}
 <center><p><button name="subject" value="setup" class="styled">Reload - Save new settings</button>&nbsp;&nbsp;&nbsp;&nbsp;<button name="subject2" value="refresh" class="styled">Refresh monitor</button></p>
 </form>
 </td></tr></table>
-<hr>
+</div>
+</td></tr></table><br></th></tr></table><p></center>
+</center></p>
+</body>
+</html>
